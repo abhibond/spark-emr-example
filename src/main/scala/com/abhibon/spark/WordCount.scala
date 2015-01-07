e7ad11c0eead4902a23efd04df21d7508d5ee3b5
abhibond <abhinav1990b@gmail.com>
2015-01-07 13:13:35 -0500

    Added WC with output to a file

--- src/main/scala/com/abhibon/spark/WordCount.scala
+++ src/main/scala/com/abhibon/spark/WordCount.scala
@@ -4,9 +4,16 @@
 package com.abhibon.spark
 
 import org.apache.spark.{SparkConf, SparkContext}
+import SparkContext._
 
 
 object WordCount {
+
+  private def tokenize(text : String) : Array[String] = {
+    // Lowercase each word and remove punctuation.
+    text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+")
+  }
+
   def main(args: Array[String]) {
     val logFile = args(0) // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
@@ -15,5 +22,8 @@ object WordCount {
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    val words = sc.textFile(args(0)).flatMap(line => tokenize(line)).map(x => (x, 1))
+    val wordCounts = words.reduceByKey(_ + _)
+    wordCounts.saveAsTextFile(args(1))
   }
 }
